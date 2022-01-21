1205421f53eaa7174c3ceb0be08e88f51b600bb9
taniguchi-123 <68776363+taniguchi-123@users.noreply.github.com>
2022-01-21 14:57:59 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+      name = 'mytestlib-123',
+      version='1.0.0',
+      url='https://estacionsuzuki.com/',
+      author = 'taniguchi-k',
+      author_email = 'kenjitaniguchi8@gmail.com',
+      description='My test library',
+      packages=find_packages()
+  )
